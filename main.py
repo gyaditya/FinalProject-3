b7ddbdf7bec7d8225d512df260b96aceb9605fed
gyaditya <112659420+gyaditya@users.noreply.github.com>
2023-01-18 09:53:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#Rock Paper Scissors By Adi
+
 import json
 import random
 
+
+#Get the Users Choice
 def getUserChoice():
     options = ["rock", "paper", "scissors"]
     userInput = input("Enter your choice (rock, paper, scissors) or press 'e' to exit the game: ").lower()
@@ -9,10 +13,12 @@ def getUserChoice():
         userInput = input("Enter your choice (rock, paper, scissors) or press 'e' to exit the game: ").lower()
     return userInput
 
+#Get the computers choice
 def getComputerChoice():
     options = ["rock", "paper", "scissors"]
     return random.choice(options)
 
+#Determine who wins
 def getRoundResult(userInput, computerChoice):
     if userInput == computerChoice:
         return "tie"
@@ -25,27 +31,37 @@ def getRoundResult(userInput, computerChoice):
     else:
         return "lose"
 
+#Play the Game
 def playGame():
+    
+    #Streak of the Current Game
     winStreak = 0
-    try:
+    
+        #Get the HighScore
         file = open("highscore.json", "r")
         dataStr = file.read()
         file.close()
         highscore = json.loads(dataStr)["highscore"]
-    except:
-        highscore = 0
         
+    #The game loop
     while True:
+        #Get the User's Input
         userInput = getUserChoice()
+        
+        #If the user Exits
         if userInput == "e":
             break
+         
+        #Get and display the Computer's choice
         computerChoice = getComputerChoice()
         print("Computer chose:", computerChoice)
-        roundResult = getRoundResult(userInput, computerChoice)
+        
+        #Get the Result of the Game
+        gameResult = getRoundResult(userInput, computerChoice)
 
-        if roundResult == "tie":
+        if gameResult == "tie":
             print("It's a tie!")
-        elif roundResult == "win":
+        elif gameResult == "win":
             print("You win!")
             winStreak += 1
         else:
@@ -54,12 +70,18 @@ def playGame():
             else:
                 print("You lost with a streak of", winStreak)
             winStreak = 0
-
+        
+        #Check if HighScore
         if winStreak > highscore:
             highscore = winStreak
             print("New high score! Your win streak is", winStreak)
+            
+            #Write to JSON
             with open("highscore.json", "w") as f:
                 json.dump({"highscore": highscore}, f)
+                
+    #Exit the Game
     print("Exiting Game")
 
+    #Run the Game
 playGame()
